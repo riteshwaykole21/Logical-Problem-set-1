af869241066723b619c16f3413a732818fa6f3e5
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-21 22:14:49 +0530

    [Ritesh]add : Print Hello

---
+++ BasicProblem/PrintHello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class PrintHello
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Ritesh Waykole");
+        }
+    }
+}
---
+++ BasicProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    PrintHello printHello = new PrintHello();
+                    printHello.Hello();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
